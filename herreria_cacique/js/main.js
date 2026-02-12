85bb2fb555e59978ccb1f01d07ff96a51da7cc8c
ConradoFrancisco <102098237+ConradoFrancisco@users.noreply.github.com>
2026-02-11 23:26:18 -0300

    cacique

---
+++ herreria_cacique/js/main.js
@@ -0,0 +1,13 @@
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+// Close menu when clicking a link
+document.querySelectorAll('.nav-links li a').forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+    });
+});
